8c427b38b301595fa30a81498470b81c74cca1dc
saiyashaswi <61510736+saiyashaswi@users.noreply.github.com>
2021-09-14 18:57:23 +0530

    kotlin programs

---
+++ kotlin_min_max.kt
@@ -0,0 +1,50 @@
+fun main(){
+  val markslist=mutableListOf(92,34,44,55,78,667,555,445,1928,334,2322,3232,2323,23333)
+  var temp:Int=markslist[0]
+ println("$markslist")   
+   for(i in 1..13){
+  
+   if (temp<markslist[i]){
+       temp=markslist[i]
+       println("the maximum value is $temp")
+println("the maximum value is $temp")
+
+
+for(i in markslist){
+  
+   if (temp>i){
+       temp=i
+       
+   }
+}
+    println("the minimum value is $temp")
+        println(markslist.contains(34))  
+            println(markslist.size)
+                println(markslist.drop(2))
+                    println(markslist.add(343))  
+                     println("$markslist")
+
+
+
+}
+
+for(i in markslist){
+  
+   if (temp>i){
+       temp=i
+       
+   }
+}
+    println("the minimum value is $temp")
+        println(markslist.contains(34))  
+            println(markslist.size)
+                println(markslist.drop(2))
+                    println(markslist.add(343))  
+                     println("$markslist")
+
+
+
+}
+   }
+}
+    
