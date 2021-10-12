e0110396accc2664f238d7d05981761f83e421f6
saiyashaswi <61510736+saiyashaswi@users.noreply.github.com>
2021-10-12 19:22:16 +0530

    patterns
    a program which prints patterns from special characters

---
+++ pattern_printing.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val rows:Int=0
+    var column:Int=0
+    for(rows in 0..2){
+        
+        for(column in 0..2){
+            print("|")
+        }
+        println("")
+    }
+}
