35b5ab787efb6bcaea2965c821426861dd053bd7
saiyashaswi <61510736+saiyashaswi@users.noreply.github.com>
2021-10-24 10:18:24 +0530

    Create rocket_pattern.kt

---
+++ rocket_pattern.kt
@@ -0,0 +1,38 @@
+fun main(){
+    
+    for(row in 0..3){
+        for(column in 0..row){
+            print("\\")
+            
+        }
+        println("")
+    }
+    for(row in 0..4){
+            
+            for(column in 0..row){
+            print("\\")
+    }
+            println("")
+    }
+    for(row in 0..5){
+            
+            for(column in 0..row){
+            print("\\")
+    }
+            println("")
+    }
+    for(row in 0..6){
+            
+            for(column in 0..row){
+            print("\\")
+    }
+            println("")
+    }
+    for(row in 0..5){
+            
+            for(column in 0..5){
+            print("^")
+    }
+            println("")
+    }
+}
