7fbe7494a8060d8186514275d4db21e2ca915016
saiyashaswi <61510736+saiyashaswi@users.noreply.github.com>
2021-10-24 10:36:29 +0530

    Create grading_app.kt

---
+++ grading_app.kt
@@ -0,0 +1,32 @@
+fun main(){
+    var marks:Int=67
+    if(marks <=100 && marks >=90){
+        println("A* excellent!!")
+    }
+    else if(marks <=89 && marks >=80){
+        println("A:very good")
+    }
+    else if(marks <=79 && marks >=70){
+        println("B good")
+    }
+    
+    else if(marks <=69 && marks >=60){
+        println(" C bad")
+    }
+    
+    else if(marks <=59 && marks >=50){
+        println("D very bad")
+    }
+    else if(marks <=49 && marks >=40){
+        println("E you should improve a lot!")
+    }
+    else if(marks <=39 && marks >=30){
+        println("F you are practising very less!")
+    }
+    else if(marks <=29 && marks >=20){
+        println("G you are practising very less!you should practise a lot.")
+    }
+    else if(marks <=19 && marks >=10){
+        println("W loser practise a lot.")
+    }
+}
